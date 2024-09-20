017b4def50b98b46903a079ca273b4307b712a81
ZhouFang92 <75120101+ZhouFang92@users.noreply.github.com>
2024-09-20 14:41:26 +0200

    add a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+   name='CRN_Simulation_Inference',
+   version='1.0.0',
+   description='CRN Simulation and Inference with Python',
+   author='Zhou Fang, Nicolo Rossi, and others',
+   author_email='zhfang@amss.ac.cn',
+   install_requires=['wheel', 'numpy', 'scipy', 'matplotlib', 'pandas'],
+   packages=setuptools.find_packages()
+)
