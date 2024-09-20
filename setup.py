b635e400166d0b265d04fee8a88fd2725af588eb
ZhouFang92 <75120101+ZhouFang92@users.noreply.github.com>
2024-09-20 15:02:29 +0200

    changes

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
    description='CRN Simulation and Inference with Python',
    author='Zhou Fang, Nicolo Rossi, and others',
    author_email='zhfang@amss.ac.cn',
-   install_requires=['wheel', 'numpy', 'scipy', 'matplotlib', 'pandas'],
+   install_requires=['wheel', 'numpy', 'scipy', 'matplotlib', 'pandas' 
+                     'igraph', 'sklearn', 'tqdm', 'inspect', 'bisect', 'joblib'],
    packages=setuptools.find_packages()
 )
